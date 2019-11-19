3b94fe572742c8fa01a8c5886a6f010ca30fb939
huseyinelmas <huseyinorkunelmas@gmail.com>
2019-11-19 11:58:12 +0300

    - added user

---
+++ app/src/main/java/com/example/studypal/User.kt
@@ -0,0 +1,10 @@
+package com.example.studypal
+
+import java.util.*
+
+data class User(
+    var username: String = "",
+    var email: String= "",
+    var birthDate: Date,
+    var phoneNumber: String?=""
+)
